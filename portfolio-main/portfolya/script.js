94f2f0058447fb7b418f30ffb98710f65404c319
Anvarbek811 <anvarrustamov811@gmail.com>
2025-07-28 15:49:01 +0500

    Add files via upload

---
+++ portfolio-main/portfolya/script.js
@@ -0,0 +1,7 @@
+// ===================typing animation=======================
+var typed = new Typed(".typing",{
+    strings:[" ","Frontend Developer", "wep Developer", "Video montaj "],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
